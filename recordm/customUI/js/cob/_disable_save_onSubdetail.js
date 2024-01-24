6fcb3b0005ee4422f9b02282d065512a91a1c648
jbarata <joao.barata@cultofbits.com>
2024-01-24 16:58:49 +0000

    also disables save-edit btn when details open

--- recordm/customUI/js/cob/_disable_save_onSubdetail.js
+++ recordm/customUI/js/cob/_disable_save_onSubdetail.js
@@ -1,11 +1,21 @@
 //--------------- Desativar "save button" da sidebar quando esta definição é aberta in-line ---------------------
-cob.custom.customize.push(function (core, utils, ui) {
+cob.custom.customize.push(function(core, utils, ui) {
     core.customizeAllInstances(function(instance, presenter) {
-        var mo = new MutationObserver(function (e) {
-            //disables sidebar save button if more than 1 is present
-            let saveButtons = document.getElementsByClassName("js-save-instance")
-            if(saveButtons.length > 0) saveButtons[0].disabled = saveButtons.length > 1
+        var mo = new MutationObserver(function(e) {
+            //disables top sidebar save button if more than 1 is present
+
+            let saveButtons = document.querySelectorAll(".js-save-instance")
+            if (saveButtons.length > 0) {
+                saveButtons[0].disabled = saveButtons.length > 1
+
+                let saveEditButtons = document.querySelectorAll(".js-save-edit-instance")
+                if (saveEditButtons.length > 0) {
+                    let refsHasSaveEdit = (document.querySelectorAll("div.js-references-new-wrapper .js-save-edit-instance").length > 0)
+                    saveEditButtons[0].disabled = (saveEditButtons.length > 1 || (saveButtons.length > 1 && !refsHasSaveEdit ))
+                }
+            }
+
         });
-        mo.observe(document.querySelector('div.instance-container'), { childList: true, subtree: true });
+        mo.observe(document.querySelector('div.js-references-new-wrapper'), {childList: true, subtree: true});
     })
 })
