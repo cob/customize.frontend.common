5d131a6bd4b85ab990ad07bf10c8dad11d0ff6af
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2024-08-12 13:47:58 +0100

    Adiciona dois novos estilos de hide hide-children, hide-expand-btn

--- recordm/customUI/js/cob/_show_hidden.js
+++ recordm/customUI/js/cob/_show_hidden.js
@@ -4,29 +4,34 @@ cob.custom.customize.push(function (core, utils, ui) {
         const urlParams = new URLSearchParams(window.location.search);
         const dev = urlParams.get('dev') || (core.getGroups() || []).includes("System");
         document.documentElement.style.setProperty("--dev-display", "");
+        document.documentElement.style.setProperty("--dev-inline-display", "");
 
-        if ( (dev == true || dev == "true")  
-            && (   $(".custom-hide").size() 
+        if ( (dev == true || dev == "true")
+            && (   $(".custom-hide").size()
                 || $(".custom-hide-in-edit").size()
-                || $(".custom-hide-in-group-edit").size() 
-                || $(".custom-hide-in-new-instance").size() 
+                || $(".custom-hide-in-group-edit").size()
+                || $(".custom-hide-in-new-instance").size()
+                || $(".custom-hide-children").size()
+                || $(".custom-disable-expand").size()
                )
             ){
 
             if(!document.getElementById("checkHidden")) {
-                var node = document.createElement('div');        
+                var node = document.createElement('div');
                 node.innerHTML = '<input type="checkbox" id="checkHidden" name="checkHidden" style="display: inline-block;margin-bottom: 4px;margin-left: -4px;">'
                                + '<label for="checkHidden" style="display: inline-block;">Show Hidden</label>';
-                        
+
                 var sidenav = document.getElementsByClassName("js-expand-all")[0];
                 sidenav.after(node);
-                
+
                 let show = false;
                 node.addEventListener ("input", function() {
                     if(show = !show) {
                         document.documentElement.style.setProperty("--dev-display", "block");
+                        document.documentElement.style.setProperty("--dev-inline-display", "inline-block");
                     } else {
                         document.documentElement.style.setProperty("--dev-display", "");
+                        document.documentElement.style.setProperty("--dev-inline-display", "");
                     }
                 });
             }
