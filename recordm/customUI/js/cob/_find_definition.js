d52bd4e75df2c75b52de005c854cf0d13e344e62
Francisco <fvpnuncio@gmail.com>
2026-03-25 11:58:11 +0000

    use kamelCase

--- recordm/customUI/js/cob/_find_definition.js
+++ recordm/customUI/js/cob/_find_definition.js
@@ -1,7 +1,7 @@
 cob.custom.customize.push(async function(core, utils, ui) {
 
-    const keyword_defId = '$auto_definitionId'
-    const keyword_defName = '$definitions'
+    const keywordDefinitionId = '$auto_definitionId'
+    const keywordDefinitions = '$definitions'
 
     const getExtension = (fp, extension) => fp.field.fieldDefinition.configuration.extensions[extension]
 
@@ -10,17 +10,17 @@ cob.custom.customize.push(async function(core, utils, ui) {
         loadDefinitions().then(definitions => {
 
             const options = definitions.map(def => `<option value="${def.name}" data-id="${def.id}">${def.name}</option>`).join("")
-            const fieldsToInject_id = presenter.findFieldPs( fp => getExtension(fp, keyword_defId))
+            const fieldsToInjectId = presenter.findFieldPs( fp => getExtension(fp, keywordDefinitionId))
 
-            if(fieldsToInject_id.length > 0){
+            if(fieldsToInjectId.length > 0){
 
                 //for each field with $auto_definitionId keyword
-                fieldsToInject_id.forEach( async fieldToInject => {
+                fieldsToInjectId.forEach( async fieldToInject => {
 
                     fieldToInject.disable() //prevent user from changing it manually
 
                     //get the argument of the keyword - field with the name of the definition
-                    const extension = getExtension(fieldToInject, keyword_defId)
+                    const extension = getExtension(fieldToInject, keywordDefinitionId)
                     const field_name = extension && extension.args ? extension.args[0] : ""
 
                     if(field_name){
@@ -43,29 +43,28 @@ cob.custom.customize.push(async function(core, utils, ui) {
             }
 
 
-            const fieldsToInject_name = presenter.findFieldPs( fp => getExtension(fp, keyword_defName))
+            const fieldsToInjectName = presenter.findFieldPs( fp => getExtension(fp, keywordDefinitions))
 
-            if(fieldsToInject_name.length > 0){
+            if(fieldsToInjectName.length > 0){
 
                 //for each field with $definitions keyword
-                fieldsToInject_name.forEach( async fieldToInject_name => {
+                fieldsToInjectName.forEach( async fieldToInjectName => {
 
-                    const $fieldTable = fieldToInject_name.content().find("> table.instance\\.service\\.field")
+                    const $fieldTable = fieldToInjectName.content().find("> table.instance\\.service\\.field")
                     const $input = $fieldTable.find(".field-value")
 
                     //if select doesn't exist yet, create it
-                    if ($fieldTable.find(`.js-select-definition[data-field-name="${fieldToInject_name.field.fieldDefinition.name}"]`).length == 0) {
+                    if ($fieldTable.find(`.js-select-definition[data-field-name="${fieldToInjectName.field.fieldDefinition.name}"]`).length == 0) {
 
                         $input.css('display', 'none')
-                        $input.after(`<select class="js-select-definition" data-field-name="${fieldToInject_name.field.fieldDefinition.name}"><option></option>${options}</select>`)
+                        $input.after(`<select class="js-select-definition" data-field-name="${fieldToInjectName.field.fieldDefinition.name}"><option></option>${options}</select>`)
 
                         const $select = $fieldTable.find(".js-select-definition")
 
-                        $select.val(fieldToInject_name.getValue())
+                        $select.val(fieldToInjectName.getValue())
 
                         $select.on('change', function() {
                             const selectedName = this.value
-                            const selectedId = $(this).find(':selected').data('id')
                             
                             $input.val(selectedName).trigger('change')
                         })
